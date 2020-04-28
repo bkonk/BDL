82a40743c61f58cea899f76393f61734494685d7
ajex1988 <ajex1988@gmail.com>
2020-04-28 15:56:54 -0400

    added triangleDataset

--- data/__init__.py
+++ data/__init__.py
@@ -1,12 +1,13 @@
 from data.gta5_dataset import GTA5DataSet
 from data.cityscapes_dataset import cityscapesDataSet
 from data.synthia_dataset import SYNDataSet
+from data.triangle_dataset import triangleDataset
 from data.cityscapes_dataset_label import cityscapesDataSetLabel
 import numpy as np
 from torch.utils import data
 
 IMG_MEAN = np.array((104.00698793, 116.66876762, 122.67891434), dtype=np.float32)
-image_sizes = {'cityscapes': (1024,512), 'gta5': (1280, 720), 'synthia': (1280, 760)}
+image_sizes = {'cityscapes': (1024,512), 'gta5': (1280, 720), 'synthia': (1280, 760),'triangle':(512,512)}
 
 def CreateSrcDataLoader(args): 
     if args.source == 'gta5':
@@ -14,7 +15,10 @@ def CreateSrcDataLoader(args):
                                     crop_size=image_sizes['gta5'], mean=IMG_MEAN)
     elif args.source == 'synthia':
         source_dataset = SYNDataSet(args.data_dir, args.data_list, max_iters=args.num_steps * args.batch_size,
-                                    crop_size=image_sizes['synthia'], mean=IMG_MEAN)  
+                                    crop_size=image_sizes['synthia'], mean=IMG_MEAN)
+    elif args.source == 'triangle':
+        source_dataset = triangleDataset(args.data_dir, args.data_list, max_iters=args.num_steps * args.batch_size,
+                                    crop_size=image_sizes['triangle'], mean=IMG_MEAN)
     else:
         raise ValueError('The target dataset mush be either gta5 or synthia')
     
@@ -23,6 +27,14 @@ def CreateSrcDataLoader(args):
     return source_dataloader
 
 def CreateTrgDataLoader(args):
+    if args.source == 'triangle':
+        # for simple triangle dataset
+        target_dataset = triangleDataset(args.data_dir_target, args.data_list_target, max_iters=args.num_steps * args.batch_size,
+                                    crop_size=image_sizes['triangle'], mean=IMG_MEAN)
+        target_dataloader = data.DataLoader(target_dataset, batch_size=args.batch_size, shuffle=True,
+                                            num_workers=args.num_workers, pin_memory=True)
+        return target_dataloader
+
     if args.data_label_folder_target is not None:
         target_dataset = cityscapesDataSetLabel(args.data_dir_target, args.data_list_target,
                                            max_iters=args.num_steps * args.batch_size,
